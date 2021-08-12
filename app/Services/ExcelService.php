792cb842149fe967fb3c479a2f238faa8b30f653
“张桂福” <904824669@qq.com>
2021-08-12 11:53:18 +0800

    导出具体到妙

--- app/Services/ExcelService.php
+++ app/Services/ExcelService.php
@@ -231,7 +231,7 @@ class ExcelService
      */
     private function _exportExcel($objWriter, $supplier = null, $fileName = null){
         header('Content-Type: applicationnd.ms-excel');
-        $time = date('Y-m-d');
+        $time = date('Y-m-d-H-i-s');
         if (!empty($supplier)) {
             $fileName = $supplier . $time . ".xls";
             header("Content-Disposition: attachment;filename=$fileName");
